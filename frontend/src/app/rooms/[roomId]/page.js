cd9ec1e74aca6ddf6f2a70c68104d2ba4717a1c0
larry <larrylongdev@gmail.com>
2023-08-13 13:28:50 -0400

    send access token with socket connection and redirect to home page on room join fail

--- frontend/src/app/rooms/[roomId]/page.js
+++ frontend/src/app/rooms/[roomId]/page.js
@@ -2,14 +2,26 @@
 
 import { useParams } from "next/navigation";
 import React, { useEffect } from "react";
+import Cookies from "js-cookies";
 import socket from "@/app/socket";
+import { useRouter } from "next/router";
 
 function Page() {
   const { roomId } = useParams();
+  const router = useRouter();
 
   useEffect(() => {
+    const accessToken = Cookies.get("accessToken");
+    socket.io.opts.extraHeaders.accessToken = accessToken;
     socket.connect();
+
     socket.emit("joinRoom", { roomId: roomId });
+
+    socket.on("joinRoomError", () => {
+      console.log(`Error joining room ${roomId}}`);
+      console.log("Returning to home page");
+      router.push("/");
+    });
 
     return () => {
       socket.disconnect();
