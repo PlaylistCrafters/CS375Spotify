ecff458ad16528c6c59242e7ffde8e49a4e383e5
brandonkhop <brandonkhop@gmail.com>
2023-08-14 00:07:08 -0400

    Implement auth code exchange for access token

--- backend/controllers/spotify-controllers.js
+++ backend/controllers/spotify-controllers.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
 const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
 const axios = require("axios");
 
 const BASE_URL = "https://api.spotify.com/v1";
-const redirect_uri = "http://localhost:3000/";
+const redirect_uri = "http://localhost:3001/callback";
 const querystring = require("node:querystring");
 
 function generateRandomString(len) {
@@ -46,6 +47,50 @@ const authorize = (req, res) => {
   );
 };
 
+const callback = async (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.code || null;
+
+  if (state === null) {
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        }),
+    );
+  } else {
+    const authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      method: "post",
+      data: querystring.stringify({
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code",
+      }),
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(`${client_id}:${client_secret}`).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+
+    axios(authOptions)
+      .then((response) => {
+        const access_token = response.data.access_token;
+
+        //TODO: we have the access token now, how do we want to pass/store it
+
+        res.redirect("http://localhost:3000/");
+      })
+      .catch((error) => {
+        console.error("Error getting access token:", error);
+        res.status(500).send("Error getting access token");
+      });
+  }
+};
+
 module.exports = {
   authorize,
+  callback,
 };
