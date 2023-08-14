27415f730856c412d37622a0a7dafa793f5152cb
brandonkhop <52135276+brandonkhop@users.noreply.github.com>
2023-08-14 17:36:02 -0400

    OAuth2 flow and Spotify request function  (#24)
    * Create router/controller for Spotify API access
    
    * Fix request urls
    
    * Configured OAuth2 flow
    
    * Implement auth code exchange for access token
    
    * Updated makeSpotifyRequest function WIP
    
    * Clean up spotify request code
    
    * Addressed PR comments
    
    * Added TODO to source redirect_uri from .env file

---
+++ backend/controllers/spotify-controllers.js
@@ -0,0 +1,138 @@
+require("dotenv").config();
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+const axios = require("axios");
+
+const BASE_URL = "https://api.spotify.com/v1";
+const redirect_uri = "http://localhost:3001/callback"; // TODO: source this from .env for dev and prod environments
+const querystring = require("node:querystring");
+
+function generateRandomString(len) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < len; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+const makeSpotifyRequest = (
+  endpoint,
+  queryParams = {},
+  body = {},
+  access_token,
+) => {
+  let url = BASE_URL + endpoint;
+
+  if (queryParams != null) {
+    url += querystring.stringify(queryParams);
+  }
+
+  let config = {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
+
+  axios
+    .get(url, config)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const authorize = (req, res) => {
+  const state = generateRandomString(16);
+  const scope = "user-read-private user-top-read";
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      }),
+  );
+};
+
+const callback = async (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.code || null;
+
+  if (state === null) {
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        }),
+    );
+  } else {
+    const authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      method: "post",
+      data: querystring.stringify({
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code",
+      }),
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(`${client_id}:${client_secret}`).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+
+    axios(authOptions)
+      .then((response) => {
+        res.cookie("accessToken", response.data.access_token, {
+          maxAge: response.data.expires_in,
+          secure: true,
+        });
+        res.redirect("/");
+      })
+      .catch((error) => {
+        console.error("Error getting access token:", error);
+        res.status(500).send("Error getting access token");
+      });
+  }
+};
+
+const clientCredentials = () => {
+  const authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    method: "post",
+    data: querystring.stringify({
+      grant_type: "client_credentials",
+    }),
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(`${client_id}:${client_secret}`).toString("base64"),
+    },
+    json: true,
+  };
+
+  axios(authOptions)
+    .then((response) => {
+      return response.data.access_token;
+    })
+    .catch((error) => {
+      return null;
+    });
+};
+
+module.exports = {
+  authorize,
+  callback,
+};
