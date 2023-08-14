6a4c18041418e7009bc95aaccc56fb637af97feb
larry <larrylongdev@gmail.com>
2023-08-13 21:07:29 -0400

    change post to get

--- backend/routes/game-routes.js
+++ backend/routes/game-routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { createRoom, getRoom } = require("../controllers/game-controllers.js");
 
 router.post("/api/rooms", createRoom);
-router.post("/api/rooms", getRoom);
+router.get("/api/rooms", getRoom);
 
 module.exports = router;
