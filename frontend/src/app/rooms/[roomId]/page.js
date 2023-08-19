cdf3c23812046a27582e5d371633ccace585eeb5
Larry <59991196+ll886@users.noreply.github.com>
2023-08-19 11:29:17 -0400

    Game frontend skeleton (#29)

--- frontend/src/app/rooms/[roomId]/page.js
+++ frontend/src/app/rooms/[roomId]/page.js
@@ -1,13 +1,27 @@
 "use client";
 
+import EndScreen from "@/components/EndScreen";
+import LobbyScreen from "@/components/LobbyScreen";
+import QuestionScreen from "@/components/QuestionScreen";
+import RoundResultsScreen from "@/components/RoundResultScreen";
 import { useParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 let socket;
 
+const lobbyScreen = "lobbyScreen";
+const questionScreen = "questionScreen";
+const roundResultsScreen = "roundResultsScreen";
+const endScreen = "endScreen";
+
 function Page() {
   const { roomId } = useParams();
+
+  const [screen, setScreen] = useState(null);
+  const [question, setQuestion] = useState(null);
+  const [players, setPlayers] = useState(null);
+  const [roundResult, setRoundResult] = useState(null);
 
   useEffect(() => {
     // TODO: Use environment variable
@@ -19,11 +33,34 @@ function Page() {
     };
   }, []);
 
-  return (
-    <div>
-      <h1>Room {roomId}</h1>
-    </div>
-  );
+  const startGameFunc = () => {
+    // TODO emit startGame event
+  };
+
+  const onSelectAnswer = () => {
+    // TODO emit answer event
+  };
+
+  const displayScreen = () => {
+    switch (screen) {
+      case lobbyScreen:
+        return <LobbyScreen players={players} startGameFunc={startGameFunc} />;
+      case questionScreen:
+        return (
+          <QuestionScreen question={question} onSelectAnswer={onSelectAnswer} />
+        );
+      case roundResultsScreen:
+        return (
+          <RoundResultsScreen players={players} roundResult={roundResult} />
+        );
+      case endScreen:
+        return <EndScreen players={players} />;
+      default:
+        return null;
+    }
+  };
+
+  return <div>{displayScreen()}</div>;
 }
 
 export default Page;
