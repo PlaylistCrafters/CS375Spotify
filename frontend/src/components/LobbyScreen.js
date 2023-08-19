bd2b305ac65ae91ac1e79305f371c784bce076bf
larry <larrylongdev@gmail.com>
2023-08-19 10:59:23 -0400

    switch argument for LobbyScreen to isHost

--- frontend/src/components/LobbyScreen.js
+++ frontend/src/components/LobbyScreen.js
@@ -1,4 +1,4 @@
-const LobbyScreen = ({ players, startGameFunc, currentPlayerId }) => {
+const LobbyScreen = ({ players, startGameFunc, isHost }) => {
   // TODO display players
   // TODO if current user is the host, then display a button to start the game
   return <div>Lobby Screen</div>;
