5888f25131dd79298419f7490637627ae72c7ad6
larry <larrylongdev@gmail.com>
2023-08-18 22:10:42 -0400

    create screen templates

---
+++ frontend/src/components/EndScreen.js
@@ -0,0 +1,6 @@
+const EndScreen = ({ players }) => {
+  // TODO display players and final rankings
+  return <div>End Screen</div>;
+};
+
+export default EndScreen;
---
+++ frontend/src/components/RoundResultScreen.js
@@ -0,0 +1,6 @@
+const RoundResultsScreen = ({ roundResult }) => {
+  // TODO display current leader board and also indicate if the current user got the answer correct
+  return <div>Round Results Screen</div>;
+};
+
+export default RoundResultsScreen;
