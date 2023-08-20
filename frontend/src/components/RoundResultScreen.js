9fa1b7e7a2b47746edf5fd467ca8ff37a98bd7e9
ljg47 <79106305+ljg47@users.noreply.github.com>
2023-08-20 19:37:12 -0400

    countdown logic

--- frontend/src/components/RoundResultScreen.js
+++ frontend/src/components/RoundResultScreen.js
@@ -1,8 +1,38 @@
 import React from "react";
 import "./RoundResultsScreen.css";
-const RoundResultsScreen = ({ players, roundResult, currentUserPlayerId }) => {
-  const sortedPlayers = players.slice().sort((a, b) => b.points - a.points);
+const RoundResultsScreen = ({ players, roundResult, currentUserPlayerId, socket }) => {
   // TODO display current leader board and also indicate if the current user got the answer correct
+  
+  const [countdownNextQuestion, setCountdownNextQuestion] = useState(30);
+  useEffect(() => {
+    let countdownNextQuestionInterval;
+
+    if (countdownNextQuestion > 0) {
+      countdownNextQuestionInterval = setInterval(() => {
+        setCountdownNextQuestion((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(countdownNextQuestionInterval);
+    };
+  }, [countdownNextQuestion]);
+
+  useEffect(() => {
+    setCountdownNextQuestion(30); 
+  }, [roundResult]);
+
+  useEffect(() => {
+    const handleNextQuestion = () => {
+    };
+    socket.on("nextQuestion", handleNextQuestion);
+    return () => {
+      socket.off("nextQuestion", handleNextQuestion);
+    };
+  }, [socket]);
+  
+  const sortedPlayers = players.slice().sort((a, b) => b.points - a.points);
+
   return (
     <div>
       <h2>Round Results</h2>
