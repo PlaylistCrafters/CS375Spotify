78606760200c211645ce4ead74ad9e83309e1299
Matt <protaciomatt@gmail.com>
2023-08-20 14:43:56 -0400

    Refactored code to use router and form

--- frontend/src/app/create/page.js
+++ frontend/src/app/create/page.js
@@ -1,45 +1,46 @@
 'use client';
 
-//TODO Function to update UI to show spotify login success
-
-function handleSubmit() {
-   let snippetLength = document.getElementsByName("sLength").value;
-   let rounds = document.getElementsByName("rounds").value;
-   let explicit = document.getElementById("explicit").value;
-   let powerups = document.getElementById("powerups").value;
-   let gameRules = [snippetLength, rounds, explicit, powerups];
-   let success = false;
-
-   fetch("http://localhost:3000/api/rooms", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({gameRules: gameRules})
-   }).then((response) => {
-        if (response.status === 200) {
-            success = true;
-        }
-        else {
-            success = false;
-        }
-        return response.json();
-   }).then((body) => {
-        document.getElementById("message").textContent = "";
-        if(success) {
-            let id = body.roomId;
-            window.location.replace("http://localhost:3000/rooms/" + id);
-        }
-        else {
-            document.getElementById("message").textContent = "Unable to create room";
-        }
-   })
-}
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+    const router = useRouter();
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        
+        let snippetLength = document.getElementsByName("sLength").value;
+        let rounds = document.getElementsByName("rounds").value;
+        let explicit = document.getElementById("explicit").value;
+        let powerups = document.getElementById("powerups").value;
+        let gameRules = [snippetLength, rounds, explicit, powerups];
+        const data = JSON.stringify({gameRules: gameRules});
+
+        const endpoint = '/api/rooms';
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: data,
+        }
+
+        const response = await fetch(endpoint, options);
+        document.getElementById("message").textContent = "";
+
+        if (response.status === 200) {
+            router.push(`/rooms/${id}`)
+        }
+        else {
+            document.getElementById("message").textcontent = "unable to create room";
+        }
+    }
+
     return (
             <div id="pageContainer">
                 <div id="settings">
                     <h1>Settings</h1>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                         <label>Snippet Length (Seconds): <input type="number" name="sLength"/></label><br/>
                         <label>Number of Rounds: <input type="number" name="rounds"/></label><br/>
                         <label>Allow Explicit Songs <select id="explicit"> 
@@ -50,8 +51,7 @@ export default function Home() {
                             <option value="yes">Yes</option>
                             <option value="no">No</option></select>
                         </label><br/>
-                        <button id="spotify" type="button">Login to Spotify</button><br/>
-                        <button id="create" type="button" onClick={handleSubmit}>Create Room</button>
+                        <button id="create" type="submit">Create Room</button>
                         </form>
                 </div>
                 <div id="message"></div>
