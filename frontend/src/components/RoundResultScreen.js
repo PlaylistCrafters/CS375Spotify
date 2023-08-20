c1ba616c8a56e5fe8756c65a3e313fcdb28827bc
ljg47 <79106305+ljg47@users.noreply.github.com>
2023-08-20 18:53:29 -0400

    leaderboard display

--- frontend/src/components/RoundResultScreen.js
+++ frontend/src/components/RoundResultScreen.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./RoundResultsScreen.css";
 const RoundResultsScreen = ({ players, roundResult }) => {
   const sortedPlayers = players.slice().sort((a, b) => b.points - a.points);
   // TODO display current leader board and also indicate if the current user got the answer correct
-  return <div>Round Results Screen</div>;
+  return (
+    <div>
+      <h2>Round Results</h2>
+      <ul>
+        {sortedPlayers.map((player) => (
+          <li key={player.id}>
+            {player.displayName}: {player.points} points
+            {player.id === currentUserPlayerId && roundResult.correctPlayers.includes(player.id) ? (
+              <span className="green-indicator">Correct</span>
+            ) : (
+              <span className="red-indicator">Incorrect</span>
+            )}
+          </li>
+        ))}
+      </ul>
+      <p>Your score: {players.find((player) => player.id === currentUserPlayerId).points}</p>
+    </div>
+  );
 };
 
 export default RoundResultsScreen;
