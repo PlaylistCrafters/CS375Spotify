7830a2f678696d772516cb8f2b246f4c412e6ec5
ljg47 <79106305+ljg47@users.noreply.github.com>
2023-08-20 18:49:50 -0400

    sorted array of players by points

--- frontend/src/components/RoundResultScreen.js
+++ frontend/src/components/RoundResultScreen.js
@@ -1,4 +1,6 @@
+import React from "react";
 const RoundResultsScreen = ({ players, roundResult }) => {
+  const sortedPlayers = players.slice().sort((a, b) => b.points - a.points);
   // TODO display current leader board and also indicate if the current user got the answer correct
   return <div>Round Results Screen</div>;
 };
