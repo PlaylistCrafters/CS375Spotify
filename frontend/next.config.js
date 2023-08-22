d55bd59e2a4692869fc0440ca6251c1240426372
Larry <59991196+ll886@users.noreply.github.com>
2023-08-22 14:38:25 -0400

    base landing page (#32)
    * add create and join buttons
    
    * add general description of game

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        SERVER_PROTOCOL: process.env.SERVER_PROTOCOL,
-        SERVER_HOST: process.env.SERVER_HOST,
-        SERVER_PORT: process.env.SERVER_PORT,
-        CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
-        CLIENT_HOST: process.env.CLIENT_HOST,
-        CLIENT_PORT: process.env.CLIENT_PORT,
-      },
+  env: {
+    SERVER_PROTOCOL: process.env.SERVER_PROTOCOL,
+    SERVER_HOST: process.env.SERVER_HOST,
+    SERVER_PORT: process.env.SERVER_PORT,
+    CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
+    CLIENT_HOST: process.env.CLIENT_HOST,
+    CLIENT_PORT: process.env.CLIENT_PORT,
+  },
 };
 
 module.exports = nextConfig;
