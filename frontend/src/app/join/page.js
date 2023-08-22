106b7718829919c3a4fcca34e24cf371b45e3dbc
mattprotacio <85465380+mattprotacio@users.noreply.github.com>
2023-08-22 14:37:31 -0400

    Initial join page, needs fetch requests implemented (#21)
    * Create router/controller for Spotify API access
    
    * Fix request urls
    
    * Initial create page
    
    * Initial join page, needs fetch requests implemented
    
    * Configured OAuth2 flow
    
    * add game route and controllers
    
    * refactor
    
    * game generation wip
    
    * send access token with socket connection and redirect to home page on room join fail
    
    * fix router and token header
    
    * remove comment
    
    * store roomId and playerId in socket object
    
    * generate questions
    
    * handle checking if room exists
    
    * change post to get
    
    * refactor endpoint GET
    
    * add spotify button to landing page
    
    * Addded initial fetch requests to the join page
    
    * Added redirect to /rooms/id
    :will
    
    * Fixed typo
    
    * Added post request sending game rules as an array value in JSON
    
    * spotify login to pull player id and display name
    
    * refactor
    
    * fix errors from merge conflicts
    
    * fix bugs around generating game
    
    * remove file
    
    * handle players leaving before game starts
    
    * set host playerId for new game
    
    * extra check against query params
    
    * pull values from env
    
    * refactor to use next/navigation
    
    * emit updateLobby event
    
    * indicate who is host
    
    * add host playerId
    
    * filter out some player data
    
    * Refactored the routing and fetch request portion of the code
    
    * Refactored code to use router and form
    
    * Pull env variables fixed fetch path
    
    * Removed direct dom manipulation and changed fetch url
    
    * Fixed typo and made response to arrow function
    
    * Fixed typo with code value
    
    ---------
    
    Co-authored-by: brandonkhop <brandonkhop@gmail.com>
    Co-authored-by: larry <larrylongdev@gmail.com>

---
+++ frontend/src/app/join/page.js
@@ -0,0 +1,41 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export default function Home() {
+    const [errorMessage, setErrorMessage] = useState('');
+    const serverProtocol = process.env.SERVER_PROTOCOL;
+    const serverHost = process.env.SERVER_HOST;
+    const serverPort = process.env.SERVER_PORT;
+
+    const router = useRouter();
+
+    const handleJoin  = async(event) => {
+        event.preventDefault();
+
+        let code = event.target.code.value;
+        const endpoint = `/api/rooms/${code}`;
+        const response = await fetch(`${serverProtocol}${serverHost}:${serverPort}${endpoint}`);
+        setErrorMessage("");
+        if (response.status === 200) {
+            router.push(`/rooms/${code}`);
+        }
+        else {
+            setErrorMessage("Invalid Room");
+        }
+    }
+
+  return (
+            <div>
+                <h1>Join Room</h1>
+                <form onSubmit={handleJoin}>
+                    <label>Room code:</label>
+                    <input id="code" type="text"/>
+                    <button id="join" type="submit">Join</button>
+                </form>
+                <div id="message">{errorMessage}
+                </div>
+            </div>
+  );
+}
