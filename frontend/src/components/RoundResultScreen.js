b33b588e545d97d7d7ed2faee90a0acab86d0d6b
ljg47 <79106305+ljg47@users.noreply.github.com>
2023-08-26 11:11:20 -0400

    Update RoundResultScreen.js

--- frontend/src/components/RoundResultScreen.js
+++ frontend/src/components/RoundResultScreen.js
@@ -1,31 +1,19 @@
-import React, { useState, useEffect } from "react";
+
 import "./RoundResultScreen.css";
 
 const RoundResultsScreen = ({ players, roundResult, currentUserPlayerId, timer }) => {
-    const [timeLeft, setTimeLeft] = useState(timer);
-    useEffect(() => {
-        setTimeLeft(timer);
-
-        const interval = setInterval(() => {
-            setTimeLeft(prevTimeLeft => Math.max(prevTimeLeft - 1, 0));
-        }, 1000);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [timer]);
 
     const sortedPlayers = players?.slice().sort((a, b) => b.points - a.points);
 
     return (
         <div>
             <h2>Round Results</h2>
-            <div>Time Left: {timeLeft} seconds</div>
+            <div>Time Left: {timer} seconds</div>
             <ul>
                 {sortedPlayers.map((player) => (
                     <li key={player.id}>
                         {player.displayName}: {player.points} points <br />
-                        {roundResult && player.id === currentUserPlayerId && roundResult.correctPlayers.includes(player.id) ? (
+                        {roundResult && player.id === currentUserPlayerId && roundResult.includes(player.id) ? (
                             <span className="green-indicator">Correct</span>
                         ) : (
                             <span className="red-indicator">Incorrect</span>
