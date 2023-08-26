046e10bf5ea646ea7e1b911f233b751a37e674d8
larry <larrylongdev@gmail.com>
2023-08-26 14:47:08 -0400

    show different ui after selecting answer and do not show player

--- frontend/src/components/QuestionScreen.js
+++ frontend/src/components/QuestionScreen.js
@@ -3,30 +3,40 @@
 import React, { useEffect, useState } from "react";
 
 const QuestionScreen = ({ question, onSelectAnswer, timer }) => {
-  console.log("from questionScreen " + question);
+  const [hasSelectedAnswer, setHasSelectedAnswer] = useState(false);
   const prompt = question["prompt"];
   const choices = question["answerChoices"];
   const url = question["songUrl"];
-  console.log(url);
-  // TODO display question and answer choices
-  // TODO call onSelectAnswer upon clicking answer choice (pass in answer string)
+
+  function selectAnswer(answer) {
+    onSelectAnswer(answer);
+    setHasSelectedAnswer(true);
+  }
+
+  if (hasSelectedAnswer) {
+    return <div>Waiting for other players...</div>;
+  }
+
   return (
-    <div>Question Screen
+    <div>
+      Question Screen
       <div>Time Left: {timer}</div>
       <div>{prompt}</div>
       <div>
-        <audio controls autoPlay>
+        <audio controls={false} autoPlay>
           <source src={url} type="audio/mpeg"></source>
         </audio>
       </div>
       <div>
-        <button onClick={() => onSelectAnswer(choices[0])}>{choices[0]}</button><br/>
-        <button onClick={() => onSelectAnswer(choices[1])}>{choices[1]}</button><br/>
-        <button onClick={() => onSelectAnswer(choices[2])}>{choices[2]}</button><br/>
-        <button onClick={() => onSelectAnswer(choices[3])}>{choices[3]}</button><br/>
-
+        <button onClick={() => selectAnswer(choices[0])}>{choices[0]}</button>
+        <br />
+        <button onClick={() => selectAnswer(choices[1])}>{choices[1]}</button>
+        <br />
+        <button onClick={() => selectAnswer(choices[2])}>{choices[2]}</button>
+        <br />
+        <button onClick={() => selectAnswer(choices[3])}>{choices[3]}</button>
+        <br />
       </div>
-
     </div>
   );
 };
