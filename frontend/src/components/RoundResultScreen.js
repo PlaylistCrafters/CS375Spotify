6b325c9f28497c2ff23df1f6270e71249c6c09db
ljg47 <79106305+ljg47@users.noreply.github.com>
2023-08-25 23:12:48 -0400

    Update RoundResultScreen.js

--- frontend/src/components/RoundResultScreen.js
+++ frontend/src/components/RoundResultScreen.js
@@ -1,6 +1,43 @@
-const RoundResultsScreen = ({ players, roundResult }) => {
-  // TODO display current leader board and also indicate if the current user got the answer correct
-  return <div>Round Results Screen</div>;
+import React, { useState, useEffect } from "react";
+import "./RoundResultScreen.css";
+
+const RoundResultsScreen = ({ players, roundResult, currentUserPlayerId, timer }) => {
+    const [timeLeft, setTimeLeft] = useState(timer);
+    useEffect(() => {
+        setTimeLeft(timer);
+
+        const interval = setInterval(() => {
+            setTimeLeft(prevTimeLeft => Math.max(prevTimeLeft - 1, 0));
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [timer]);
+
+    const sortedPlayers = players?.slice().sort((a, b) => b.points - a.points);
+
+    return (
+        <div>
+            <h2>Round Results</h2>
+            <div>Time Left: {timeLeft} seconds</div>
+            <ul>
+                {sortedPlayers.map((player) => (
+                    <li key={player.id}>
+                        {player.displayName}: {player.points} points <br />
+                        {roundResult && player.id === currentUserPlayerId && roundResult.correctPlayers.includes(player.id) ? (
+                            <span className="green-indicator">Correct</span>
+                        ) : (
+                            <span className="red-indicator">Incorrect</span>
+                        )}
+                    </li>
+                ))}
+            </ul>
+            {currentUserPlayerId && (
+                <p>Your score: {players.find((player) => player.id === currentUserPlayerId)?.points}</p>
+            )}
+        </div>
+    );
 };
 
 export default RoundResultsScreen;
