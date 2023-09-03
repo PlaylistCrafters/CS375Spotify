4b47727b526aa4c068f5550a80a42dfece5005ed
brandonkhop <brandonkhop@gmail.com>
2023-09-03 17:56:43 -0400

    Set color on username

--- frontend/src/components/LobbyScreen.js
+++ frontend/src/components/LobbyScreen.js
@@ -1,6 +1,27 @@
 import styles from "./LobbyScreen.module.css";
+import React, { useEffect, useState } from "react";
 
 const LobbyScreen = ({ players, startGameFunc, isHost, roomId }) => {
+  const [playerColors, setPlayerColors] = useState([]);
+
+  const setRandomColor = () => {
+    const colors = [
+      "#f6454d",
+      "#9267ff",
+      "#46c6f7",
+      "#50ca8d",
+      " #ffd452",
+      " #ff9352",
+    ];
+    var randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  useEffect(() => {
+    const randomColors = players.map(() => setRandomColor());
+    setPlayerColors(randomColors);
+  }, [players]);
+
   return (
     <main>
       <div>
@@ -15,7 +36,11 @@ const LobbyScreen = ({ players, startGameFunc, isHost, roomId }) => {
       </div>
       <ul>
         {players.map((player, index) => (
-          <li className={styles.user} key={index}>
+          <li
+            className={styles.user}
+            style={{ color: playerColors }}
+            key={index}
+          >
             {player.displayName}
           </li>
         ))}
