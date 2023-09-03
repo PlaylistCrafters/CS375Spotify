38d2b82508266290630044958d9d93ea6fea38b7
mattprotacio <85465380+mattprotacio@users.noreply.github.com>
2023-09-02 23:24:34 -0400

    Styling for the create and join page (#46)

--- frontend/src/app/create/page.js
+++ frontend/src/app/create/page.js
@@ -1,7 +1,14 @@
 "use client";
 
+import { Ubuntu } from "next/font/google";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import styles from "./Create.module.css";
+
+const ubuntu = Ubuntu({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+});
 
 export default function Home() {
   const [errorMessage, setErrorMessage] = useState("");
@@ -14,20 +21,25 @@ export default function Home() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    let errorSound = new Audio('https://vgmsite.com/soundtracks/nintendo-switch-sound-effects/phmumiwe/Error.mp3');
+    let settingsSound = new Audio('https://vgmsite.com/soundtracks/nintendo-switch-sound-effects/udkyavrh/Settings.mp3');
     const snippetLength = event.target.sLength.value;
     const rounds = event.target.rounds.value;
     const allowExplicit = event.target.explicit.value;
 
     if (snippetLength < 15 || snippetLength > 30) {
         setErrorMessage("Snippet length must be between 15-30 seconds");
+        errorSound.play();
         return;
     }
     if (rounds < 1 || rounds > 10) {
-        setErrorMessage("The amount of arounds must be between 1-10");
+        setErrorMessage("The amount of rounds must be between 1-10");
+        errorSound.play();
         return;
     }
     if (allowExplicit !== "yes" && allowExplicit !== "no") {
         setErrorMessage("The explicit value must be Yes or No");
+        errorSound.play();
         return;
     }
 
@@ -58,28 +70,27 @@ export default function Home() {
     setErrorMessage("");
 
     if (response.status === 200) {
+    settingsSound.play();
       response.json().then((body) => {
         router.push(`/rooms/${body.roomId}`);
       });
     } else {
-      setErrorMessage("Unable to create Room");
+        errorSound.play();
+        setErrorMessage("Unable to create Room");
     }
   };
 
   return (
-    <div id="pageContainer">
+    <main className={ubuntu.className}>
+    <div className={styles.wrapper}>
       <div id="settings">
-        <h1>Settings</h1>
+        <div className={styles.header}>Settings</div>
         <form onSubmit={handleSubmit}>
-          <label>
-            Snippet Length (Seconds): <input type="number" name="sLength" />
-          </label>
+            <input className={styles.textInput} id={styles.snippetLabel} type="number" name="sLength" placeholder="Snippet Length (Seconds)"/>
           <br />
-          <label>
-            Number of Rounds: <input type="number" name="rounds" />
-          </label>
+            <input className={styles.textInput} id={styles.roundLabel} type="number" name="rounds" placeholder="Number of Rounds"/>
           <br />
-          <label>
+          <label className={styles.dropdownInput} id={styles.explicitLabel}>
             Allow Explicit Songs{" "}
             <select id="explicit">
               <option value="yes">Yes</option>
@@ -87,7 +98,7 @@ export default function Home() {
             </select>
           </label>
           <br />
-          <label>
+          <label className={styles.dropdownInput} id={styles.powerupsLabel}>
             Powerups{" "}
             <select id="powerups">
               <option value="yes">Yes</option>
@@ -95,12 +106,13 @@ export default function Home() {
             </select>
           </label>
           <br />
-          <button id="create" type="submit">
+          <button className={styles.create} id="create" type="submit">
             Create Room
           </button>
         </form>
       </div>
-      <div id="message" style={{color: 'red'}}>{errorMessage}</div>
+      <div className={styles.error} id="message">{errorMessage}</div>
     </div>
+    </main>
   );
 }
